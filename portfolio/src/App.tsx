1889cc239f304958afccf90897ec3c418111af11
gunda manoharreddy <gundamanoharreddy15@gmail.com>
2026-02-24 17:46:55 +0530

    manohar

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -2,11 +2,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -16,12 +14,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <HashRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </HashRouter>
+        <Index />
       </TooltipProvider>
     </QueryClientProvider>
   </HelmetProvider>
