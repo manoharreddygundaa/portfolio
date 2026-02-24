f63736605a1dc07e8b028c001132e52af065ddd6
gunda manoharreddy <gundamanoharreddy15@gmail.com>
2026-02-24 17:26:27 +0530

    manohar

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "https://github.com/manoharreddygundaa/portfolio", // your repo name
+  base: "/portfolio/", // your repo name
 
   server: {
     host: "::",
